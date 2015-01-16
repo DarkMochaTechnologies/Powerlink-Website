7b4e80eb58c5e1bcd311c86b90841b0e3bc70ad0
LouizEchi <louizechiverri@gmail.com>
2015-01-16 16:54:40 +0800

    template clean up

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -19,35 +19,13 @@ class Home extends BMS_Controller
 			base_url() . 'application/public/js/jquery.lettering.js',
 			base_url() . 'application/public/js/jquery-2.1.1.min.js',
 			);
-		$this->load->library('form_validation');
 		$this->load->helper('form');
 		$this->load->library('session');
 	} //end of function __construct
 
 	function index(){	
 		$this->load->helper('file');
-		$this->load->model('admin_model');
-		$this->load->model('member_model');
 
-		$this->form_validation->set_rules('txt_username', 'CategoryID', 'trim|required|xss_clean|min_length[3]');
-		$this->form_validation->set_rules('txt_password', 'Member_Pword', 'trim|required|xss_clean|min_length[4]');
-		
-		if($this->form_validation->run()){
-			$o_user_account = $this->admin_model->login();
-			if($o_user_account != FALSE){
-				$o_user_info = $this->member_model->get_info($o_user_account->MemberID);
-				$a_data = array('Username' => $o_user_account->Username, 
-								'AccNo' => $o_user_account->AccNo,
-								'AccType' => $o_user_account->AccType,
-								'FirstName' => $o_user_info->Mem_FirstName,
-								'LastName' => $o_user_info->Mem_LastName,
-								);
-				$this->session->set_userdata('AccInfo', $a_data);
-				$this->data['o_user'] = $o_user_info;
-			}else{
-				$this->data['s_error'] = "Invalid Username/Password, Please try again";
-			}
-		}
 		$this->data['s_main_content'] = 'home';
 		$this->load->view('includes/template', $this->data);
 	} //end of function index
--- application/views/admin/get_member_info.php
+++
@@ -1,7 +0,0 @@
- <?php
-           foreach($a_o_member_list as $o_member_info){
- ?>
-            <li class = "row-sidebar"><a class="btn btn-transparent btn-li" href="<?=base_url()?>admin/view_user_info/<?=$o_member_info->Mem_ID?>"><h2><?=$o_member_info->Mem_FirstName?></h2></a></li> 
-          <?php
-            }
- ?>
