1d94ccadcdc33bb3093e073303e3a74bc98fa1a4
LouizEchi <louizechiverri@gmail.com>
2015-01-21 21:00:21 +0800

    fdgdfg

--- application/views/home.php
+++ application/views/home.php
@@ -1,8 +1,13 @@
 
     <div class ="carousel-custom">
-        <div><img src ="<?=base_url()?>application/public/img/main/admin_background.jpg"/> </div>
-        <div><img src ="<?=base_url()?>application/public/img/main/bms_background.jpg"/> </div>
-         <div><img src ="<?=base_url()?>application/public/img/main/admin_background2.jpg"/> </div>
+        <div><img src ="<?=base_url()?>application/public/img/main/admin_background.jpg" class="c-images"/> </div>
+        <div><img src ="<?=base_url()?>application/public/img/main/bms_background.jpg" class="c-images"/> </div>
+         <div><img src ="<?=base_url()?>application/public/img/main/admin_background2.jpg" class="c-images"/> </div>
+         <ul class="slick-dots" style="display:block;">
+           <li class><button type="button" data-role="none">1</button></li>
+            <li class><button type="button" data-role="none">2</button></li>
+             <li class><button type="button" data-role="none">3</button></li>
+         </ul>
     </div>
     <script type="text/javascript">
         $(document).ready(function(){
